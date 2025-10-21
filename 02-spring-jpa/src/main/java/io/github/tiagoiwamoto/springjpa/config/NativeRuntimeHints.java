e91f9d503d491adb9c716b46a9bc306c9c783e3a
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2025-10-21 08:52:00 -0300

    hints

--- 02-spring-jpa/src/main/java/io/github/tiagoiwamoto/springjpa/config/NativeRuntimeHints.java
+++ 02-spring-jpa/src/main/java/io/github/tiagoiwamoto/springjpa/config/NativeRuntimeHints.java
@@ -1,72 +1,54 @@
 package io.github.tiagoiwamoto.springjpa.config;
 
-import io.github.tiagoiwamoto.springjpa.entity.Jogo;
-import org.hibernate.Hibernate;
-import org.hibernate.proxy.HibernateProxy;
+import org.hibernate.bytecode.internal.none.BytecodeProviderImpl;
+import org.hibernate.dialect.PostgreSQLDialect;
+import org.hibernate.dialect.type.PostgreSQLArrayJdbcType;
+import org.hibernate.dialect.type.PostgreSQLArrayJdbcTypeConstructor;
+import org.hibernate.dialect.type.PostgreSQLCastingInetJdbcType;
+import org.hibernate.dialect.type.PostgreSQLCastingIntervalSecondJdbcType;
+import org.hibernate.dialect.type.PostgreSQLCastingJsonArrayJdbcType;
+import org.hibernate.dialect.type.PostgreSQLCastingJsonArrayJdbcTypeConstructor;
+import org.hibernate.dialect.type.PostgreSQLCastingJsonJdbcType;
+import org.hibernate.dialect.type.PostgreSQLEnumJdbcType;
+import org.hibernate.dialect.type.PostgreSQLInetJdbcType;
+import org.hibernate.dialect.type.PostgreSQLIntervalSecondJdbcType;
+import org.hibernate.dialect.type.PostgreSQLJsonArrayPGObjectJsonJdbcTypeConstructor;
+import org.hibernate.dialect.type.PostgreSQLJsonArrayPGObjectJsonbJdbcTypeConstructor;
+import org.hibernate.dialect.type.PostgreSQLJsonArrayPGObjectType;
+import org.hibernate.dialect.type.PostgreSQLJsonPGObjectJsonType;
+import org.hibernate.dialect.type.PostgreSQLJsonPGObjectJsonbType;
+import org.hibernate.dialect.type.PostgreSQLOrdinalEnumJdbcType;
+import org.hibernate.dialect.type.PostgreSQLStructCastingJdbcType;
+import org.hibernate.dialect.type.PostgreSQLStructPGObjectJdbcType;
+import org.hibernate.dialect.type.PostgreSQLUUIDJdbcType;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-
-import java.util.List;
 
 public class NativeRuntimeHints implements RuntimeHintsRegistrar {
 
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-        // JDBC / Dialect
-        hints.resources().registerType(org.hibernate.dialect.PostgreSQLDialect.class);
-        hints.reflection().registerType(
-                org.hibernate.dialect.PostgreSQLDialect.class,
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS
-        );
-
-        // Alguns tipos do Hibernate usados via service/strategy
-        List<Class<?>> hibernateTypes = List.of(
-                org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl.class,
-                org.hibernate.engine.jdbc.env.spi.JdbcEnvironment.class,
-                org.hibernate.boot.model.relational.ColumnOrderingStrategyStandard.class
-        );
-        for (Class<?> t : hibernateTypes) {
-            hints.reflection().registerType(
-                    t,
-                    MemberCategory.INVOKE_PUBLIC_METHODS,
-                    MemberCategory.INVOKE_DECLARED_CONSTRUCTORS
-            );
-        }
-
-        // Registrar proxy/hint do Hibernate
-        hints.reflection().registerType(Hibernate.class, MemberCategory.INVOKE_PUBLIC_METHODS);
-        hints.reflection().registerType(HibernateProxy.class, MemberCategory.INVOKE_PUBLIC_METHODS);
-
-        // Registrar entidades e inner classes (ex.: builders)
-        registerAnnotatedTypes(hints, "io.github.tiagoiwamoto.springjpa.entity", Jogo.class);
-        // registro explícito do builder interno se existir
-        try {
-            Class<?> builder = Class.forName("io.github.tiagoiwamoto.springjpa.entity.Jogo$JogoBuilder");
-            hints.reflection().registerType(
-                    builder,
-                    MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
-                    MemberCategory.ACCESS_DECLARED_FIELDS,
-                    MemberCategory.INVOKE_PUBLIC_METHODS
-            );
-        } catch (ClassNotFoundException ignored) {}
-    }
-
-    private void registerAnnotatedTypes(RuntimeHints hints, String basePackage, Class<?> annotation) {
-        var scanner = new ClassPathScanningCandidateComponentProvider(false);
-        for (BeanDefinition bd : scanner.findCandidateComponents(basePackage)) {
-            try {
-                Class<?> type = Class.forName(bd.getBeanClassName());
-                hints.reflection().registerType(
-                        type,
-                        MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
-                        MemberCategory.INVOKE_PUBLIC_METHODS,
-                        MemberCategory.ACCESS_DECLARED_FIELDS,
-                        MemberCategory.INVOKE_PUBLIC_METHODS
-                );
-            } catch (ClassNotFoundException ignored) {}
-        }
+        hints.reflection()
+                .registerType(PostgreSQLDialect.class, MemberCategory.values())
+                .registerType(PostgreSQLArrayJdbcType.class, MemberCategory.values())
+                .registerType(PostgreSQLArrayJdbcTypeConstructor.class, MemberCategory.values())
+                .registerType(PostgreSQLCastingInetJdbcType.class, MemberCategory.values())
+                .registerType(PostgreSQLCastingIntervalSecondJdbcType.class, MemberCategory.values())
+                .registerType(PostgreSQLCastingJsonArrayJdbcType.class, MemberCategory.values())
+                .registerType(PostgreSQLCastingJsonArrayJdbcTypeConstructor.class, MemberCategory.values())
+                .registerType(PostgreSQLCastingJsonJdbcType.class, MemberCategory.values())
+                .registerType(PostgreSQLEnumJdbcType.class, MemberCategory.values())
+                .registerType(PostgreSQLInetJdbcType.class, MemberCategory.values())
+                .registerType(PostgreSQLIntervalSecondJdbcType.class, MemberCategory.values())
+                .registerType(PostgreSQLJsonArrayPGObjectJsonbJdbcTypeConstructor.class, MemberCategory.values())
+                .registerType(PostgreSQLJsonArrayPGObjectJsonJdbcTypeConstructor.class, MemberCategory.values())
+                .registerType(PostgreSQLJsonArrayPGObjectType.class, MemberCategory.values())
+                .registerType(PostgreSQLJsonPGObjectJsonbType.class, MemberCategory.values())
+                .registerType(PostgreSQLJsonPGObjectJsonType.class, MemberCategory.values())
+                .registerType(PostgreSQLOrdinalEnumJdbcType.class, MemberCategory.values())
+                .registerType(PostgreSQLStructCastingJdbcType.class, MemberCategory.values())
+                .registerType(PostgreSQLStructPGObjectJdbcType.class, MemberCategory.values())
+                .registerType(PostgreSQLUUIDJdbcType.class, MemberCategory.values());
     }
 }
